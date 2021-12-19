f334b95e9351d421d8541abf0a3b5609e34c665c
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-19 18:43:57 +0100

    Prepare for heroku deplyoment

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
 
 // const app = http.createServer((request, response) => {
