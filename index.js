a657cabac3e30b8aeb2affd4cc5b0d03113812c3
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-21 13:28:46 +0100

    Add part3 exercise 3.15. solution

--- index.js
+++ index.js
@@ -106,10 +106,16 @@ app.get("/api/persons/:id", (request, response) => {
 
 // Delete a resource
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  // const id = Number(request.params.id);
+  // persons = persons.filter((person) => person.id !== id);
+  // response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 // Add a resource
